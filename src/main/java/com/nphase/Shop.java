6fc15e736dbe8f48bd28b7f9b1760def4a5395cc
Sebastian Cielma <cielmasebastian@gmail.com>
2023-07-31 14:36:00 +0200

    Task 1

--- src/main/java/com/nphase/Shop.java
+++ src/main/java/com/nphase/Shop.java
@@ -4,15 +4,14 @@ import com.nphase.entity.Product;
 import com.nphase.entity.ShoppingCart;
 import com.nphase.service.ShoppingCartService;
 
+import java.awt.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class Shop {
     public static void main(String[] args) {
-        ShoppingCartService shoppingCartService = new ShoppingCartService();
-
-        Product tea = new Product("tea", new BigDecimal(5),1);
-        Product coffee = new Product("coffee", new BigDecimal(3.5),2);
+        Product tea = new Product("tea", new BigDecimal("5"), 1);
+        Product coffee = new Product("coffee", new BigDecimal("3.5"), 2);
 
         ArrayList<Product> productList = new ArrayList<>();
         productList.add(tea);
@@ -20,7 +19,9 @@ public class Shop {
 
         ShoppingCart shoppingCart = new ShoppingCart(productList);
 
-        shoppingCartService.calculateTotalPrice(shoppingCart);
+        ShoppingCartService shoppingCartService = new ShoppingCartService();
+
+        BigDecimal totalPrice = shoppingCartService.calculateTotalPrice(shoppingCart);
 
     }
 }
